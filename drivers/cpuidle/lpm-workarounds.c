ChenZesen/SimpleK_Santoni_V2

Add all these drivers the flag POWER_EFFICIENT to try to save some power and less cpu heating

drivers/cpuidle/lpm-workarounds.c
@@ -68,7 +68,7 @@ static int lpm_wa_cx_unvote_init(struct platform_device *pdev)
 	INIT_WORK(&dummy_vote_work, send_dummy_cx_vote);
 
 	lpm_wa_wq = alloc_workqueue("lpm-wa",
-				WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_HIGHPRI, 1);
+				WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_POWER_EFFICIENT | WQ_HIGHPRI, 1);
 
 	return ret;
 }

drivers/platform/msm/ipa/ipa_v2/ipa_dp.c
@@ -1184,7 +1184,7 @@ int ipa2_setup_sys_pipe(struct ipa_sys_connect_params *sys_in, u32 *clnt_hdl)
 		snprintf(buff, IPA_RESOURCE_NAME_MAX, "ipawq%d",
 				sys_in->client);
 		ep->sys->wq = alloc_workqueue(buff,
-				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_UNBOUND, 1);
+				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_POWER_EFFICIENT | WQ_UNBOUND, 1);
 		if (!ep->sys->wq) {
 			IPAERR("failed to create wq for client %d\n",
 					sys_in->client);
@@ -1195,7 +1195,7 @@ int ipa2_setup_sys_pipe(struct ipa_sys_connect_params *sys_in, u32 *clnt_hdl)
 		snprintf(buff, IPA_RESOURCE_NAME_MAX, "iparepwq%d",
 				sys_in->client);
 		ep->sys->repl_wq = alloc_workqueue(buff,
-				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_UNBOUND, 1);
+				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_POWER_EFFICIENT | WQ_UNBOUND, 1);
 		if (!ep->sys->repl_wq) {
 			IPAERR("failed to create rep wq for client %d\n",
 					sys_in->client);

Nicklas373/kernel_xiaomi_msm8953-3.18-2

drivers/cpuidle/lpm-workarounds.c
@@ -68,7 +68,7 @@ static int lpm_wa_cx_unvote_init(struct platform_device *pdev)
 	INIT_WORK(&dummy_vote_work, send_dummy_cx_vote);
 
 	lpm_wa_wq = alloc_workqueue("lpm-wa",
-				WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_HIGHPRI, 1);
+				WQ_UNBOUND | WQ_MEM_RECLAIM | WQ_HIGHPRI | WQ_POWER_EFFICIENT, 1);
 
 	return ret;
 }

drivers/platform/msm/ipa/ipa_v2/ipa_dp.c
@@ -1187,7 +1187,7 @@ int ipa2_setup_sys_pipe(struct ipa_sys_connect_params *sys_in, u32 *clnt_hdl)
 		snprintf(buff, IPA_RESOURCE_NAME_MAX, "ipawq%d",
 				sys_in->client);
 		ep->sys->wq = alloc_workqueue(buff,
-				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_UNBOUND, 1);
+				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_UNBOUND | WQ_POWER_EFFICIENT, 1);
 		if (!ep->sys->wq) {
 			IPAERR("failed to create wq for client %d\n",
 					sys_in->client);
@@ -1198,7 +1198,7 @@ int ipa2_setup_sys_pipe(struct ipa_sys_connect_params *sys_in, u32 *clnt_hdl)
 		snprintf(buff, IPA_RESOURCE_NAME_MAX, "iparepwq%d",
 				sys_in->client);
 		ep->sys->repl_wq = alloc_workqueue(buff,
-				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_UNBOUND, 1);
+				WQ_HIGHPRI | WQ_MEM_RECLAIM | WQ_UNBOUND | WQ_POWER_EFFICIENT, 1);
 		if (!ep->sys->repl_wq) {
 			IPAERR("failed to create rep wq for client %d\n",
 					sys_in->client);