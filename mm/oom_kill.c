b34e1239bb496a69ae7b73bd4b130f89d5e54d0d
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-12-02 18:32:26 +0700

    Revert "mm, oom_kill: Use new totalram_pages() API"
    This reverts commit e81a0e5a04a979f7226d53a232527f99bfb6bdb8.

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -249,7 +249,7 @@ static enum oom_constraint constrained_alloc(struct zonelist *zonelist,
 				gfp_t gfp_mask, nodemask_t *nodemask,
 				unsigned long *totalpages)
 {
-	*totalpages = totalram_pages() + total_swap_pages;
+	*totalpages = totalram_pages + total_swap_pages;
 	return CONSTRAINT_NONE;
 }
 #endif
