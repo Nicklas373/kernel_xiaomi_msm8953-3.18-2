f8aa29c8b9312a29e25827ccfd60ede0b3abaa88
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-12-02 18:32:43 +0700

    Revert "fs: Convert remaining users of total{ram,high}_pages to atomic"
    This reverts commit 8fff8d07cf92660abb35dd0518554e6c8f0ab8f4.

--- fs/file_table.c
+++ fs/file_table.c
@@ -446,7 +446,7 @@ void put_filp(struct file *file)
 }
 
 void __init files_init(unsigned long mempages)
-{
+{ 
 	unsigned long n;
 
 	filp_cachep = kmem_cache_create("filp", sizeof(struct file), 0,
@@ -454,11 +454,11 @@ void __init files_init(unsigned long mempages)
 
 	/*
 	 * One file with associated inode and dcache is very roughly 1K.
-	 * Per default don't use more than 10% of our memory for files.
-	 */
+	 * Per default don't use more than 10% of our memory for files. 
+	 */ 
 
 	n = (mempages * (PAGE_SIZE / 1024)) / 10;
 	files_stat.max_files = max_t(unsigned long, n, NR_FILE);
 	percpu_counter_init(&nr_files, 0, GFP_KERNEL);
 	global_filetable_print_warning_once();
-}
+} 
